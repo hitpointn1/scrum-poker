53009ee7642395f570e1f932f15967d49a80ea9f
Malezhina Irina <malezhina.irina@yandex.ru>
2019-04-16 01:34:59 +0300

    Добавлено редактирование/удаление задачи; список онлайн пользователей; отображение, что пользователь уже проголосовал; валидация на задачу; выход пользователей из сети.

--- ScrumPoker/PlanningPoker/Hubs/VotingHub.cs
+++ ScrumPoker/PlanningPoker/Hubs/VotingHub.cs
@@ -1,20 +1,57 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using PlanningPoker.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace PlanningPoker
 {
     public class VotingHub : Hub
     {
+        public void JoinGroup(string groupName)
+        {
+            this.Groups.AddToGroupAsync(this.Context.ConnectionId, groupName);
+        }
+
         public override async Task OnConnectedAsync()
         {
+            await Clients.All.SendAsync("EntranceUser");
+           // await Clients.Caller.SendAsync("UpdatePage"); //при подключении другого пользователя - обновление (для отображения списка онлайн)
             await base.OnConnectedAsync();
         }
+
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            await Clients.All.SendAsync("ExitUser");
             await base.OnDisconnectedAsync(exception);
+        }
+
+        public void OnlineUser(int UserId)
+        {
+            using (var _context = new PokerPlanningContext())
+            {
+                var Player = _context.Players.Where(p => p.Id == UserId).SingleOrDefault();
+                Player.IsOnline = true;
+
+                _context.Players.Update(Player);
+                _context.SaveChanges();
+            }
+        }
+
+        public void OfflineUser(int RoomId)
+        {
+            using (var _context = new PokerPlanningContext())
+            {
+                var Player = _context.Players.Where(p => p.PokerRoomId == RoomId).ToList();
+                foreach (var player in Player)
+                {
+                    player.IsOnline = false;
+                }
+                _context.Players.UpdateRange(Player);
+                _context.SaveChanges();
+            }
         }
     }
 }
